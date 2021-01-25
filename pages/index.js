2e5d5db142979c66453ba4d323810df1c858ec08
paulafalves <paula.alves@elysios.com.br>
2021-01-25 20:19:54 -0300

    End of the first class

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Footer from '../src/components/Footer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>{db.description}</Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h2>Quiz da Galera</h2>
+          </Widget.Header>
+          <Widget.Content>aqui vai texto</Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/paulafalves" />
+    </QuizBackground>
+  )
 }
